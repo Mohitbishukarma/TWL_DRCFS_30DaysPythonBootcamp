951c653c556ee14cf6e23eda4e52661394f1be5e
Mohit B.K <bkmohit6@gmail.com>
2022-12-15 20:11:09 +0545

    add day5 of week 2

---
+++ Week 2/mohitbk/day5/main.py
@@ -0,0 +1,23 @@
+# with open('file.txt', 'w') as f:
+#     with open('file2.txt','w') as f2:
+#         f2.write("dskjhf")
+#     f.write("dshh")
+
+
+# dir fucntion : only works in REPL
+# import random
+# dir(random)
+
+# with open('file.txt', 'w') as f:
+#     with open('file2.txt','w') as f2:
+#         f2.write("dskjhf")
+#         # TODO: Do some code
+         
+#     f.write("dshh")
+
+
+# any ....> or gate
+# all .....> and gate
+
+
+from typing import Any
